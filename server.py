24f2e07fa32de1adeee56474b6b057992cf41b62
Logan Vining <llvinin@hotmail.com>
2018-09-19 20:37:52 -0600

    Hit/Miss Message Encoding

--- server.py
+++ server.py
@@ -7,6 +7,21 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 # Get Port from Command Line Argument
 PORT = int(sys.argv[1])
 
+#init own board
+own_board = []
+
+# Function to initialize the player's own_board
+def init_board():
+
+    #put own board into 2D list
+    with open('own_board.txt', 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            own_board.append(list(line))
+
+
 # Class that handles incoming fire messages from client
 class BattleshipHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -17,27 +32,29 @@ class BattleshipHTTPRequestHandler(BaseHTTPRequestHandler):
         coord_list = re.findall(r'\d', str(post_data))
         x = int(coord_list[0])
         y = int(coord_list[1])
+
+        hit = 0
+        if own_board[y][x] != '_':
+            hit = 1
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        message = "hit"
-        self.wfile.write(message)
+        message = ("hit=" + str(hit)).encode('utf-8')
+        print(hit)
+        self.wfile.write(bytes(message))
 
-#init own board
-own_board = []
 
-#put own board into 2D list
-with open('own_board.txt', 'r') as file:
-    while True:
-        line = file.readline()
-        if not line:
-            break
-        own_board.append(list(line))
-
+# Function that initializes and runs the actual server process
 def run(server_class = HTTPServer, handler_class = BattleshipHTTPRequestHandler):
     server_address = ('', PORT)
     httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
 
+init_board()
 run()
